c5469959c6562c462552862ed4377fbdd598ba1d
Yury <yury_alekseev@yahoo.com>
2016-05-01 20:33:01 +0300

    Add LisLenght nlogn implementation.

---
+++ lis.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+namespace util {
+
+/* O(nlogn) solution */
+template <typename Iterator>
+int LisLength(Iterator begin, Iterator end) {
+    if (begin == end) {
+        return 0;
+    }
+
+    std::vector<int> t;
+    t.reserve(std::distance(begin, end));
+    t.push_back(*begin);
+
+    for (auto it = std::next(begin); it != end; ++it) {
+        auto boundIt = std::lower_bound(t.begin(), t.end(), *it);
+        if (boundIt == t.end()) {
+            t.push_back(*it);
+        } else {
+            *boundIt = *it;
+        }
+    }
+
+    return t.size();
+}
+
+} // namespace util
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "lis.h"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> a = { 10, 9, 2, 5, 3, 7, 101, 18 };
+
+    std::cout << util::LisLength(a.begin(), a.end()) << std::endl;
+
+    return 0;
+}
